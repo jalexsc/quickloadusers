ebf2bf7f8873358e0faa212afc63d6d247dbb7dc
Jose Alexander Soto Cortez <jalexsc@gmail.com>
2022-05-20 12:56:30 -0400

    delete report

--- main_AcqErm_local_nust.py
+++ main_AcqErm_local_nust.py
@@ -13,15 +13,13 @@ if __name__ == "__main__":
     #Insert the customer c-ode here or enter the customer running the code, by default blank
     
     customerName="uai"
-    migrationreport=mr.MigrationReport()
-    migrationreport.add_general_statistics("Alex test")
     
     #Insert True if do you want to get the reference data from server, this function allow download the reference data to acquisitions, by default TRUE
     getrefdata=True#False
     #Insert True/False if you want to use GUI / command line. by default command Line FALSE
     graphicinterfaces=False#False
     #Insert the script to run (o=organizations/p=purchase orders/l=licenses/a=Agreements)
-    scriptTorun="l"#"o"    
+    scriptTorun="u"#"o"    
     if customerName=="": 
         print("Client code: ") 
         customerName = str(input())
